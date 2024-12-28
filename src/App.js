43c3fced85cf01957988b6fc59337857575e1245
Diego Torres <ditola@gmail.com>
2024-12-28 07:31:37 -0500

    fist commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Portfolio from './components/portfolio.js';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
